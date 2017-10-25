c25abc24937f50ac78b66046ae4777430d46180f
Julien DAUPHANT <judauphant@gmail.com>
2017-10-25 17:16:26 +0200

    Add temporary persistance + interaction

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -3,6 +3,11 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models._
+
+import org.joda.time.DateTime
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,22 +15,60 @@ import play.api.mvc._
  */
 @Singleton
 class ApplicationController @Inject()(implicit val webJarAssets: WebJarAssets) extends Controller {
+  val sabineAuthor = "Sabine, Assistante Sociale de la ville d'Argenteuil"
+
+  var applications = List(
+    Application(
+      "1",
+      "En cours",
+      DateTime.now(),
+      sabineAuthor,
+      "Etat dossier CAF de Mr MARTIN John",
+      "Bonjour,\nMr MARTIN John a fait transférer son dossier de la CAF de Marseille à la CAF d'Argenteuil, il ne sait pas où en est sa demande et voudrait savoir à qui envoyer ces documents de suivie.\nSon numéro à la CAF de Marseille est le 98767687, il est né le 16 juin 1985.\n\nMerci de votre aide",
+      "Forte"),
+    Application(
+      "0",
+      "Terminé",
+      DateTime.now(),
+      sabineAuthor,
+      "Demande d'APL de Mme DUPOND Martine",
+      "Bonjour,\nMme DUPOND Martine né le 12 juin 1978 a déposé une demande d'APL le 1 octobre.\nPouvez-vous m'indiquez l'état de sa demande ?\n\nMerci de votre réponse",
+      "Moyenne")
+  )
 
 
   def create = Action { implicit request =>
     Ok(views.html.createApplication())
   }
 
+  case class ApplicatonData(subject: String, description: String, priority: String)
+  val applicationForm = Form(
+    mapping(
+      "subject" -> text,
+      "description" -> text,
+      "priority" -> text
+    )(ApplicatonData.apply)(ApplicatonData.unapply)
+  )
+
+
   def createPost = Action { implicit request =>
+    val applicationData = applicationForm.bindFromRequest.get
+    val application = Application(applications.length.toString, "En cours", DateTime.now(), sabineAuthor, applicationData.subject, applicationData.description, applicationData.priority)
+    applications = application :: applications
     Redirect(routes.ApplicationController.all()).flashing("success" -> "Votre demande a bien été envoyé")
   }
 
   def all = Action { implicit request =>
-    Ok(views.html.allApplication())
+    Ok(views.html.allApplication(applications))
   }
 
-  def show = Action { implicit request =>
-    Ok(views.html.showApplication())
+  def show(id: String) = Action { implicit request =>
+    applications.find(_.id == id) match {
+      case None =>
+        NotFound("")
+      case Some(application) =>
+        Ok(views.html.showApplication(application))
+    }
   }
 
   def answer = Action { implicit request =>
---
+++ app/models/Application.scala
@@ -0,0 +1,11 @@
+package models
+
+import org.joda.time.DateTime
+
+case class Application(id: String,
+                       status: String,
+                       creationDate: DateTime,
+                       author: String,
+                       subject: String,
+                       description: String,
+                       priority: String)
