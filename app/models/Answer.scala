b0267b563de1bb72a5df9a427e387213a6b5490e
Julien DAUPHANT <judauphant@gmail.com>
2017-10-25 18:38:22 +0200

    Add comments in memory

---
+++ app/models/Answer.scala
@@ -0,0 +1,16 @@
+package models
+
+import org.joda.time.DateTime
+
+case class Answer(applicationId: String, creationDate: DateTime, message: String, author: User, users: List[User], last: Boolean)
+
+object Answer {
+  var all = List(
+    Answer("0", DateTime.now(), "Je transmets pour info à la CAF", User.all.filter(_.qualite == "DILA").head, User.all.filter(_.qualite == "CAF"), false),
+    Answer("0", DateTime.now(), "Le dossier de Mme DUPOND a été accepté, elle devrait recevoir une réponse par courrier bientôt.", User.all.filter(_.qualite == "CAF").head, List(), true)
+  )
+  def forApplicationId(id: String) = all.filter(_.applicationId == id)
+  def add(answer: Answer): Unit = {
+    all = all ++ List(answer)
+  }
+}
--- app/models/User.scala
+++ app/models/User.scala
@@ -9,6 +9,7 @@ case class User(id: String,
 object User {
   val all = List(
     User("sabine", "Sabine", "Assistante Sociale de la ville d'Argenteuil", "sabine@example.com", false),
+    User("dila", "Zohra", "DILA", "zohra@example.com", true),
     User("jean", "Jean DUCAFE", "CAF", "jean@example.com", true),
     User("paul", "Paul MURSSAF", "URSSAF", "paul@example.com", true),
     User("amelie", "Amelie LASANTE", "CPAM", "sabine@example.com", true)
