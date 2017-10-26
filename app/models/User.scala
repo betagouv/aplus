b357f15e27b58e4c0c3f5ad1edeaa142fc8bdf11
LEQUEPEYS Dominique <dlequepeys@octo.com>
2017-10-26 08:09:20 +0200

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -8,11 +8,11 @@ case class User(id: String,
 
 object User {
   val all = List(
-    User("sabine", "Sabine", "Assistante Sociale de la ville d'Argenteuil", "sabine@example.com", false),
-    User("dila", "Zohra", "DILA", "zohra@example.com", true),
-    User("jean", "Jean DUCAFE", "CAF", "jean@example.com", true),
-    User("paul", "Paul MURSSAF", "URSSAF", "paul@example.com", true),
-    User("amelie", "Amelie LASANTE", "CPAM", "sabine@example.com", true)
+    User("sabine", "Sabine", "Assistante Sociale de la ville d'Argenteuil", "sabine@assistante-sociale.fr", false),
+    User("dila", "Zohra", "DILA", "zohra@dila.gouv.fr", true),
+    User("jean", "Jean DUCAFE", "CAF", "jean@caf.fr", true),
+    User("paul", "Paul MURSSAF", "URSSAF", "paul@ursaff.fr", true),
+    User("amelie", "Amelie LASANTE", "CPAM", "sabine@cpam.fr", true)
   )
   def get(id: String): Option[User] = all.find(_.id == id)
 }
