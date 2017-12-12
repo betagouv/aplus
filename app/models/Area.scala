e7094542b49e3149db28a580ab007389dc17265c
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2017-12-12 16:21:00 +0100

    Ajouts de nouvelles zones

--- app/models/Area.scala
+++ app/models/Area.scala
@@ -10,6 +10,10 @@ object Area {
   val all = List(
     Area(UUIDHelper.namedFrom("argenteuil"), "Argenteuil"),
     Area(UUIDHelper.namedFrom("besancon"), "Besançon"),
+    Area(UUIDHelper.namedFrom("lons-le-saunoer"), "Lons-le-Saunier"),
+    Area(UUIDHelper.namedFrom("perigueux"), "Périgueux"),
+    Area(UUIDHelper.namedFrom("angers"), "Angers"),
+    Area(UUIDHelper.namedFrom("nice"), "Nice"),
     Area(UUIDHelper.namedFrom("exemple"), "Demo")
   )
 }
