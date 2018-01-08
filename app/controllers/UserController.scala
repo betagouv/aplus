99fc28f4df4411ad17728b65063ccbd2caf83bec
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2018-01-08 18:28:34 +0100

    Amélioré sécurité Utilisateurs

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -21,7 +21,11 @@ class UserController @Inject()(loginAction: LoginAction,
                                notificationsService: NotificationService)(implicit val webJarsUtil: WebJarsUtil) extends InjectedController with play.api.i18n.I18nSupport {
 
   def all = loginAction { implicit request =>
-    Ok(views.html.allUsers(request.currentUser, request.currentArea)(userService.byArea(request.currentArea.id)))
+    if(request.currentUser.admin != true) {
+      Unauthorized("Vous n'avez pas le droit de faire ça")
+    } else {
+      Ok(views.html.allUsers(request.currentUser, request.currentArea)(userService.byArea(request.currentArea.id)))
+    }
   }
 
   private val timeZone = DateTimeZone.forID("Europe/Paris")
@@ -53,50 +57,66 @@ class UserController @Inject()(loginAction: LoginAction,
   )
 
   def edit = loginAction { implicit request =>
-    implicit val area = request.currentArea
-    val users = userService.allDBOnlybyArea(area.id)
-    val form = usersForm.fill(users)
-    Ok(views.html.editUsers(request.currentUser, request.currentArea)(form, users.length, routes.UserController.editPost()))
+    if(request.currentUser.admin != true) {
+      Unauthorized("Vous n'avez pas le droit de faire ça")
+    } else {
+      implicit val area = request.currentArea
+      val users = userService.allDBOnlybyArea(area.id)
+      val form = usersForm.fill(users)
+      Ok(views.html.editUsers(request.currentUser, request.currentArea)(form, users.length, routes.UserController.editPost()))
+    }
   }
 
   def editPost = loginAction { implicit request =>
-    implicit val area = request.currentArea
-    usersForm.bindFromRequest.fold(
-      formWithErrors => {
-        BadRequest(views.html.editUsers(request.currentUser, request.currentArea)(formWithErrors, 0, routes.UserController.editPost()))
-      },
-      users => {
-        if(users.foldRight(true)({ (user, result) => userService.update(user) && result})) {
-          Redirect(routes.UserController.all()).flashing("success" -> "Modification sauvegardé")
-        } else {
-          val form = usersForm.fill(users).withGlobalError("Impossible de mettre à jour certains utilisateurs (Erreur interne)")
-          InternalServerError(views.html.editUsers(request.currentUser, request.currentArea)(form, users.length, routes.UserController.editPost()))
+    if(request.currentUser.admin != true) {
+      Unauthorized("Vous n'avez pas le droit de faire ça")
+    } else {
+      implicit val area = request.currentArea
+      usersForm.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest(views.html.editUsers(request.currentUser, request.currentArea)(formWithErrors, 0, routes.UserController.editPost()))
+        },
+        users => {
+          if (users.foldRight(true)({ (user, result) => userService.update(user) && result })) {
+            Redirect(routes.UserController.all()).flashing("success" -> "Modification sauvegardé")
+          } else {
+            val form = usersForm.fill(users).withGlobalError("Impossible de mettre à jour certains utilisateurs (Erreur interne)")
+            InternalServerError(views.html.editUsers(request.currentUser, request.currentArea)(form, users.length, routes.UserController.editPost()))
+          }
         }
-      }
-    )
+      )
+    }
   }
 
   def add = loginAction { implicit request =>
-    implicit val area = request.currentArea
-    val rows = request.getQueryString("rows").map(_.toInt).getOrElse(1)
-    Ok(views.html.editUsers(request.currentUser, request.currentArea)(usersForm, rows, routes.UserController.addPost()))
+    if(request.currentUser.admin != true) {
+      Unauthorized("Vous n'avez pas le droit de faire ça")
+    } else {
+      implicit val area = request.currentArea
+      val rows = request.getQueryString("rows").map(_.toInt).getOrElse(1)
+      Ok(views.html.editUsers(request.currentUser, request.currentArea)(usersForm, rows, routes.UserController.addPost()))
+    }
   }
 
   def addPost = loginAction { implicit request =>
-    implicit val area = request.currentArea
-    usersForm.bindFromRequest.fold(
-      formWithErrors => {
-        BadRequest(views.html.editUsers(request.currentUser, request.currentArea)(formWithErrors, 0, routes.UserController.addPost()))
-      },
-      users => {
-          if(userService.add(users)) {
+    if(request.currentUser.admin != true) {
+      Unauthorized("Vous n'avez pas le droit de faire ça")
+    } else {
+      implicit val area = request.currentArea
+      usersForm.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest(views.html.editUsers(request.currentUser, request.currentArea)(formWithErrors, 0, routes.UserController.addPost()))
+        },
+        users => {
+          if (userService.add(users)) {
             Redirect(routes.UserController.all()).flashing("success" -> "Utilisateurs ajouté")
           } else {
             val form = usersForm.fill(users).withGlobalError("Impossible d'ajouté les utilisateurs (Erreur interne)")
             InternalServerError(views.html.editUsers(request.currentUser, request.currentArea)(form, users.length, routes.UserController.addPost()))
           }
-      }
-    )
+        }
+      )
+    }
   }
 
   def showCharte() = loginAction { implicit request =>
