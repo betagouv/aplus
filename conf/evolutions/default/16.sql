8c28d057d0c56af62a97a4903b4f5054d5f47f00
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2019-03-29 17:49:53 +0100

    Stocker les ips dans les événements

--- conf/evolutions/default/16.sql
+++ conf/evolutions/default/16.sql
@@ -6,8 +6,8 @@ CREATE TABLE login_token (
      expiration_date timestamp with time zone NOT NULL,
      ip_address inet NOT NULL
 );
-ALTER TABLE event ADD ip_address inet DEFAULT NOT NULL;
+ALTER TABLE "event" ADD ip_address inet DEFAULT '::1'::inet NOT NULL;
 
 # --- !Downs
-ALTER TABLE event DROP commune_code;
+ALTER TABLE "event" DROP ip_address;
 DROP TABLE login_token;
