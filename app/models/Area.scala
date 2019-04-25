38fffcc4a9c1052081d7e226ff8077148a7a4772
Julien DAUPHANT <jdauphant@users.noreply.github.com>
2019-04-25 10:55:42 +0200

    Update Area.scala

--- app/models/Area.scala
+++ app/models/Area.scala
@@ -13,11 +13,18 @@ object Area {
     Area(UUIDHelper.namedFrom("angers"), "Angers"),
     Area(UUIDHelper.namedFrom("cahors"), "Bassin de Cahors"),
     Area(UUIDHelper.namedFrom("besancon"), "Besançon"),
+    Area(UUIDHelper.namedFrom("bethune"), "Bethune"),
     Area(UUIDHelper.namedFrom("coeur-du-perche"), "Cœur du Perche"),
+    Area(UUIDHelper.namedFrom("doubs"), "Doubs"),
+    Area(UUIDHelper.namedFrom("ecouen"), "Écouen"),
     Area(UUIDHelper.namedFrom("hauts-de-seine"),"Hauts-de-Seine"),
+    Area(UUIDHelper.namedFrom("jura"), "Jura"),
+    Area(UUIDHelper.namedFrom("loir-et-cher"), "Loir-et-Cher"),
     Area(UUIDHelper.namedFrom("lons-le-saunoer"), "Lons-le-Saunier"),
+    Area(UUIDHelper.namedFrom("lyon"), "Lyon"),
     Area(UUIDHelper.namedFrom("perigueux"), "Périgueux"),
     Area(UUIDHelper.namedFrom("val-de-marne"), "Val-de-Marne"),
+    Area(UUIDHelper.namedFrom("var"), "Var"),
     Area(UUIDHelper.namedFrom("exemple"), "Demo")
   )
 }
