eeb120c18530f94c94812a1953d2108540880c96
Sylvain <sylvain.dermy@gmail.com>
2019-06-18 16:52:04 +0200

    Disable Louis

--- app/models/User.scala
+++ app/models/User.scala
@@ -35,9 +35,9 @@ object User {
     // Enabled
     User(UUIDHelper.namedFrom("zohra"), Hash.sha256(s"zohra"), "Zohra LEBEL", "Experte A+", "zohra.lebel@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, expert = true, cguAcceptationDate = Some(date)),
     User(UUIDHelper.namedFrom("julien"), Hash.sha256(s"julien"), "Julien DAUPHANT", "Admin A+", "julien.dauphant@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, cguAcceptationDate = Some(date)),
-    User(UUIDHelper.namedFrom("louis"), Hash.sha256(s"louis"), "Louis MOSCAROLA", "Expert A+", "louis.moscarola@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, expert = true, cguAcceptationDate = Some(date)),
     User(UUIDHelper.namedFrom("sylvain"), Hash.sha256(s"sylvain"), "Sylvain DERMY", "Expert A+", "sylvain.dermy@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, expert = true, cguAcceptationDate = Some(date)),
     // Disabled
+    User(UUIDHelper.namedFrom("louis"), Hash.sha256(s"louis - disabled"), "Louis MOSCAROLA (disabled)", "Expert A+", "louis.moscarola@beta.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("yan"), Hash.sha256(s"yan - disabled"), "Yan TANGUY (disabled)", "Aide A+", "yan.tanguy@dila.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("pierre"), Hash.sha256(s"pierre -disabled"), "Pierre MOMBOISSE (disabled)", "Aide A+", "pierre.momboisse@beta.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("simon"), Hash.sha256(s"simon - disabled"), "Simon PINEAU (disabled)", "Aide A+", "simon.pineau@beta.gouv.fr - disabled", true, false, false, List(), date, false, "75056", false),
