175241342a4c49fda5e21014c4a4c711b85a59c4
Sylvain <sylvain.dermy@gmail.com>
2019-08-01 14:07:51 +0200

    Update User.scala

--- app/models/User.scala
+++ app/models/User.scala
@@ -36,9 +36,9 @@ object User {
     User(UUIDHelper.namedFrom("zohra"), Hash.sha256(s"zohra"), "Zohra LEBEL", "Experte A+", "zohra.lebel@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, expert = true, cguAcceptationDate = Some(date)),
     User(UUIDHelper.namedFrom("julien"), Hash.sha256(s"julien"), "Julien DAUPHANT", "Admin A+", "julien.dauphant@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, cguAcceptationDate = Some(date)),
     User(UUIDHelper.namedFrom("sylvain"), Hash.sha256(s"sylvain"), "Sylvain DERMY", "Expert A+", "sylvain.dermy@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, true, "75056", true, expert = true, cguAcceptationDate = Some(date)),
-    User(UUIDHelper.namedFrom("simon"), Hash.sha256(s"simon"), "Simon PINEAU", "Expert A+", "simon.pineau@beta.gouv.fr", true, false, true, Area.all.map(_.id), date, false, "75056", true, expert = true, cguAcceptationDate = Some(date)),
    // Disabled
-    User(UUIDHelper.namedFrom("louis"), Hash.sha256(s"louis - disabled"), "Louis MOSCAROLA (disabled)", "Expert A+", "louis.moscarola@beta.gouv.fr", false, false, false, List(), date, false, "75056", false),
+    User(UUIDHelper.namedFrom("simon"), Hash.sha256(s"simon - disabled"), "Simon PINEAU", "Expert A+ (disabled)", "simon.pineau@beta.gouv.fr - disabled", false, false, true, Area.all.map(_.id), date, false, "75056", true, expert = true, cguAcceptationDate = Some(date)),
+    User(UUIDHelper.namedFrom("louis"), Hash.sha256(s"louis - disabled"), "Louis MOSCAROLA (disabled)", "Expert A+", "louis.moscarola@beta.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("yan"), Hash.sha256(s"yan - disabled"), "Yan TANGUY (disabled)", "Aide A+", "yan.tanguy@dila.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("pierre"), Hash.sha256(s"pierre -disabled"), "Pierre MOMBOISSE (disabled)", "Aide A+", "pierre.momboisse@beta.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
     User(UUIDHelper.namedFrom("dominique"), Hash.sha256(s"dominique - disabled"), "Dominique LEQUEPEYS (disabled)", "Aide A+", "dominique.lequepeys@beta.gouv.fr - disabled", false, false, false, List(), date, false, "75056", false),
