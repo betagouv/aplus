aa9b967bd2606c9efdfe76441231d2bf40747387
Julien DAUPHANT <jdauphant@users.noreply.github.com>
2019-08-19 11:03:08 +0200

    Ajout token CSRF (Sécurité) + 6 tests automatisés (#202)
    * Improve CGU
    
    * Suppression .get sur option
    
    * Improve CSS
    
    * Ajout token CSRF sur tout les formulaires
    
    * Ajout de test sur la connexion
    
    * Improve circleci
    
    * Ajout Coverage
    
    * Ajout de test pour les chemins du HomeController
    
    * Ajout de test de token
    
    * Amélioration Circle CI
    
    * Ajout Codacy
    
    * Clean tests code

---
+++ test/browser/BaseSpec.scala
@@ -0,0 +1,11 @@
+package browser
+
+import play.api.inject.guice.GuiceApplicationBuilder
+
+trait BaseSpec {
+  def applicationWithBrowser = {
+    new GuiceApplicationBuilder()
+      .configure("app.filesPath" -> "files", "app.host" -> "localhost", "play.mailer.mock" -> true)
+      .build()
+  }
+}
---
+++ test/browser/LoginSpec.scala
@@ -0,0 +1,73 @@
+package browser
+
+import extentions.{Time, UUIDHelper}
+import javax.inject.Inject
+import models.LoginToken
+import org.joda.time.DateTime
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+import play.api.test._
+import play.api.test.Helpers._
+import services.TokenService
+
+
+@RunWith(classOf[JUnitRunner])
+class LoginSpec extends Specification with Tables with BaseSpec {
+
+  "Login" should {
+    "Login with valid or invalid emails" in new WithBrowser(webDriver = WebDriverFactory(HTMLUNIT), app = applicationWithBrowser) {
+        "email"                              | "result"                                             |
+        "julien.dauphant" + "@beta.gouv.fr"  ! "Regardez vos emails"                                |
+        "wrong@beta.gouv.fr"                 ! "Il n'y a pas d'utilisateur avec cette adresse email"|>
+          { (email, expected) =>
+            val loginURL = controllers.routes.LoginController.login().absoluteURL(false, s"localhost:$port")
+
+            browser.goTo(loginURL)
+            browser.el("input[name='email']").fill().withText(email)
+            browser.el("form").submit()
+
+            browser.pageSource must contain(expected)
+        }
+    }
+
+    "Use token with success"  in new WithBrowser(webDriver = WebDriverFactory(HTMLUNIT), app = applicationWithBrowser) {
+      val tokenService = app.injector.instanceOf[TokenService]
+      val loginToken = LoginToken.forUserId(UUIDHelper.namedFrom("julien"), 5, "127.0.0.1")
+      tokenService.create(loginToken)
+
+      val loginURL = controllers.routes.LoginController.redirect().absoluteURL(false, s"localhost:$port")
+
+      browser.goTo(s"$loginURL?token=${loginToken.token}&path=/")
+
+      eventually {
+        browser.url must endWith(controllers.routes.ApplicationController.all().url.substring(1))
+      }
+    }
+    "Use expired token without sucess"  in new WithBrowser(webDriver = WebDriverFactory(HTMLUNIT), app = applicationWithBrowser) {
+      val tokenService = app.injector.instanceOf[TokenService]
+      val loginToken = LoginToken.forUserId(UUIDHelper.namedFrom("julien"), 5, "127.0.0.1")
+        .copy(expirationDate = DateTime.now(Time.dateTimeZone).minusMinutes(5))
+      tokenService.create(loginToken)
+
+      val loginURL = controllers.routes.LoginController.redirect().absoluteURL(false, s"localhost:$port")
+
+      browser.goTo(s"$loginURL?token=${loginToken.token}&path=/")
+
+      eventually {
+        browser.url must endWith(controllers.routes.LoginController.login().url.substring(1))
+        browser.pageSource must contain("Le lien que vous avez utilisez a expiré (il expire après")
+      }
+    }
+    "Use token without success"  in new WithBrowser(webDriver = WebDriverFactory(HTMLUNIT), app = applicationWithBrowser) {
+      val loginURL = controllers.routes.LoginController.redirect().absoluteURL(false, s"localhost:$port")
+
+      browser.goTo(s"$loginURL?token=90798798789798&path=/")
+
+      eventually {
+        browser.url must endWith(controllers.routes.LoginController.login().url.substring(1))
+        browser.pageSource must contain("Le lien que vous avez utilisé n'est plus valide, il a déjà été utilisé.")
+      }
+    }
+  }
+}
