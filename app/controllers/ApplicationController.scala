2bf8c6b0496e9f5f7a7dde1dcafae37bd0825658
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2019-10-11 18:33:55 +0200

    Ajout d'un test de création de demande

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -89,7 +89,7 @@ class ApplicationController @Inject()(loginAction: LoginAction,
            formWithErrors => {
              // binding failure, you retrieve the form containing errors:
              val instructors = userService.byArea(request.currentArea.id).filter(_.instructor)
-             eventService.info("APPLICATION_CREATION_INVALID", s"L'utilisateur essai de créé une demande invalide")
+             eventService.info(s"APPLICATION_CREATION_INVALID", s"L'utilisateur essai de créé une demande invalide $formWithErrors")
              val groupIds = instructors.flatMap(_.groupIds).distinct
              if(simplified) {
                val categories = organisationService.categories
