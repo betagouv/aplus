bcb08cb9e2e5fa4df0620efd39337a9a1d049069
Lucien Pereira <lucien.pereira@colabrain.fr>
2019-11-04 11:31:05 +0100

    [144] Échappement des données pour l'export CSV

--- app/extentions/CSVUtil.scala
+++ app/extentions/CSVUtil.scala
@@ -1,8 +1,34 @@
 package extentions
 
+import models.{Application, Area, User}
+
 object CSVUtil {
 
   def escape(content: String): String = {
-    "\"" + content.replace("\"", "\"\"") + "\""
+    "\"" + content.filterNot(_ == '\n').replace("\"", "\"\"") + "\""
+  }
+
+  def getCSVLineFor(application: Application, users: List[User], requestedByAdmin: Boolean): String = {
+    val id = application.id
+    val status = application.status
+    val created = application.creationDate
+    val creatorName = escape(application.creatorUserName)
+    val creatorId = application.creatorUserId
+    val subject = escape(application.subject)
+    val invited = escape(application.invitedUsers.values.mkString(","))
+    val administrations = escape(application.administrations(users).mkString(","))
+    val area = escape(Area.all.find(_.id == application.area).map(_.name).head)
+    val closed = application.closedDate.getOrElse("")
+    val adminSpecific = if (requestedByAdmin) {
+      val irrelevant = if (application.irrelevant) "Oui"
+      else "Non"
+      irrelevant + ";" + application.usefulness.getOrElse("?")
+    } else {
+      ""
+    }
+    val answer = application.firstAnswerTimeInMinutes.map(_.toString).getOrElse("")
+    val resolution = application.resolutionTimeInMinutes.map(_.toString).getOrElse("")
+
+    s"""$id;$status;$created;$creatorName;$creatorId;$subject;$invited;$administrations;$area;$closed;$adminSpecific;$answer;$resolution"""
   }
 }
