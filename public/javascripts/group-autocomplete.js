42d9bcbb42164b4996f761c2656405f55b48d7b1
Lucien Pereira <lucien.pereira@colabrain.fr>
2019-11-13 19:24:40 +0100

    [151] En temps qu'Administrateur, je veux pouvoir sélectionner plusieurs zones géographique pour un groupe.

--- public/javascripts/group-autocomplete.js
+++ public/javascripts/group-autocomplete.js
@@ -22,28 +22,28 @@ var Group = {
         });
     },
     displayArea: function (element) {
-        var label = document.createElement('label');
+        var label = document.createElement("label");
         label.textContent = element.name;
-        var closeButton = document.createElement('input');
+        var closeButton = document.createElement("input");
         closeButton.setAttribute("type", "button");
         closeButton.name=element.code
         closeButton.value = "X";
         closeButton.addEventListener("click", function (event) {
             Group.remove(element.code);
         });
-        var input = document.createElement('input');
+        var input = document.createElement("input");
         input.setAttribute("class", "mdl-textfield__input area");
         input.setAttribute("type", "hidden");
         input.setAttribute("name", "insee-code[]");
         input.setAttribute("id", "insee-code-label-"+element.code);
         input.setAttribute("value", element.code);
         input.setAttribute("readonly", "");
-        var li = document.createElement('li');
+        var li = document.createElement("li");
         li.appendChild(label);
         li.appendChild(closeButton);
         li.appendChild(input);
         Group.container.appendChild(li);
-        Group.input.value = '';
+        Group.input.value = "";
     },
     init: function(data) {
         var iterable = JSON.parse(data);
@@ -61,11 +61,11 @@ window.document.addEventListener("DOMContentLoaded", function (event) {
     "use strict";
     Group.container = document.getElementById("group-area-container");
     Group.input = document.getElementById("insee-code");
-    Group.init(Group.input.getAttribute("data-lang"))
+    Group.init(Group.input.getAttribute("data-lang"));
 
     // initialize
     var my_autoComplete = new autoComplete({
-        selector: 'input[id="insee-code"]',
+        selector: "input[id=\"insee-code\"]",
         minChars: 2,
         source: function(term, response) {
             function reqListener() {
@@ -78,17 +78,17 @@ window.document.addEventListener("DOMContentLoaded", function (event) {
             }
             var oReq = new XMLHttpRequest();
             oReq.addEventListener("load", reqListener);
-            var url = 'https://joplus.herokuapp.com/search/?query='+term
+            var url = "https://joplus.herokuapp.com/search/?query="+term
             oReq.open("GET", url);
             oReq.send();
         }, renderItem: function (item, search) {
-            search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
-            var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+            search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+            var re = new RegExp("(" + search.split(" ").join("|") + ")", "gi");
             return '<div class="autocomplete-suggestion" data-lang="'+item[1]+'" data-langname="'+item[0]+'" data-val="'+search+'">'+(item[0]+ " ("+item[2]+" / "+item[1]+")").replace(re, "<b>$1</b>")+'</div>';
         }, onSelect: function(e, term, item) {
             var element = {
-                name: item.getAttribute('data-langname'),
-                code: item.getAttribute('data-lang')
+                name: item.getAttribute("data-langname"),
+                code: item.getAttribute("data-lang")
             }
             Group.addArea(element);
         }
