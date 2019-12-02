48149ef5bb0a06411f54e0bddc057f3023f1d418
Lucien Pereira <lucien.pereira@colabrain.fr>
2019-12-02 09:32:45 +0100

    Pouvoir mettre plusieurs pièces jointes lors des demandes/réponses.

--- public/javascripts/application-attachment.js
+++ public/javascripts/application-attachment.js
@@ -3,29 +3,30 @@ ApplicationAttachment = {};
 window.document.addEventListener("DOMContentLoaded", function (event) {
     "use strict";
     ApplicationAttachment.list = document.getElementById("attachment-list");
-    ApplicationAttachment.currentCount = 1;
-    ApplicationAttachment.maxId = 0;
-    ApplicationAttachment.inputChangedEventListener = function (event) {
-        if(event.target.files.length > 0) {
-            // Add one line.
+    ApplicationAttachment.fileInputCount = 1;
+    ApplicationAttachment.idSequence = 0; // In order to avoid id collisions
+    ApplicationAttachment.increaseOrDecreaseFileInputList = function (event) {
+        if (event.target.files.length > 0) {
+            // If a new file is added, add a new file input.
             var li = document.createElement("li");
             var input = document.createElement("input");
             input.setAttribute("type", "file");
-            input.setAttribute("name", "file["+(ApplicationAttachment.maxId++)+"]");
-            ApplicationAttachment.currentCount++;
-            input.addEventListener("change", ApplicationAttachment.inputChangedEventListener);
+            input.setAttribute("name", "file["+(ApplicationAttachment.idSequence++)+"]");
+            ApplicationAttachment.fileInputCount++;
+            input.addEventListener("change", ApplicationAttachment.increaseOrDecreaseFileInputList);
             ApplicationAttachment.list.appendChild(li);
             li.appendChild(input);
         } else {
-            // Remove this line if it's not the ultimate.
-            if(ApplicationAttachment.currentCount > 1) {
+            // If a previous file is unset for upload, remove the corresponding input file.
+            if (ApplicationAttachment.fileInputCount > 1) {
                 event.target.parentElement.parentElement.removeChild(event.target.parentElement);
-                ApplicationAttachment.currentCount--;
+                ApplicationAttachment.fileInputCount--;
             }
         }
     };
-    var length = ApplicationAttachment.list.childNodes.length;
+    // For all file inputs, add a listener that adds a new file input to allow
+    // for more attachments to be made.
     ApplicationAttachment.list.querySelectorAll("input").forEach(function (input) {
-        input.addEventListener("change", ApplicationAttachment.inputChangedEventListener);
+        input.addEventListener("change", ApplicationAttachment.increaseOrDecreaseFileInputList);
     });
 }, false);
