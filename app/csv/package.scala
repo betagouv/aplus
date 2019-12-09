15c718769e3e33774b95a924f86e868960231c9f
Julien DAUPHANT <julien.dauphant@beta.gouv.fr>
2019-12-09 18:07:39 +0100

    Change convention de nommage des groupes

--- app/csv/package.scala
+++ app/csv/package.scala
@@ -188,9 +188,9 @@ package object csv {
     val groupToUsersMap = deduplicatedEmail.groupBy(_._1.name).map({ case (_, tuple) => tuple.head._1 -> tuple.map(_._2) }) // Group by group name
     groupToUsersMap -> lineNumberToErrors
   }
-
+  
   private def prepareGroup(group: UserGroup, area: Area): UserGroup = {
-    group.copy(name = area.name + ":" + group.name, area = area.id)
+    group.copy(name = s"${group.name} - ${area.name}", area = area.id)
   }
 
   private def prepareUsers(users: List[User], group: UserGroup): List[User] = {
