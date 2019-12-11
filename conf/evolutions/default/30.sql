693e896269b0d73fed9ec89bb2cf43c88224ae62
Lucien Pereira <lucienpereiramailbox+bitbucket@gmail.com>
2019-12-11 16:09:07 +0100

    [286] Mettre le nom de groupe à 60 caractéres.

--- conf/evolutions/default/30.sql
+++ conf/evolutions/default/30.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(60) NOT NULL;
+ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(60);
 
 # --- !Downs
-ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(50) NOT NULL;
+ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(50);
