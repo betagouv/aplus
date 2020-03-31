d6bef125abf79603028d57e98f0bb3d9cd6072e5
niladic <git@nil.choron.cc>
2020-03-31 13:58:02 +0200

    Add tooltip on application subject.

--- public/javascripts/main-page-bottom.js
+++ public/javascripts/main-page-bottom.js
@@ -9,3 +9,30 @@ var slimSelectClass = "use-slimselect"
 Array.from(document.querySelectorAll("." + slimSelectClass)).forEach(function (select) {
   new SlimSelect({ select: select })
 });
+
+
+//
+// Fix Accessibility for tooltips
+//
+// see https://stackoverflow.com/questions/45364249/tooltip-accessibility
+// https://www.w3.org/TR/wai-aria-practices/#tooltip
+// https://github.com/nico3333fr/van11y-accessible-simple-tooltip-aria/blob/master/src/van11y-accessible-simple-tooltip-aria.es6.js
+// https://github.com/google/material-design-lite/blob/mdl-1.x/src/tooltip/tooltip.js
+// TODO: the tooltip should be visible when there is a focus event
+
+Array.from(document.querySelectorAll(".mdl-tooltip")).forEach(function (tooltip) {
+  var elementId = tooltip.getAttribute("for") || tooltip.getAttribute("data-mdl-for");
+  if (elementId) {
+    var element = document.getElementById(elementId);
+    ["mouseenter", "focus"].forEach(function (eventName) {
+      element.addEventListener(eventName, function (e) {
+        tooltip.setAttribute("aria-hidden", "false");
+      });
+    });
+    ["mouseleave", "blur"].forEach(function (eventName) {
+      element.addEventListener(eventName, function (e) {
+        tooltip.setAttribute("aria-hidden", "true");
+      });
+    });
+  }
+});
