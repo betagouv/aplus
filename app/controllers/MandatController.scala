3edfbfdcaf246ce765db5c1c14abd756e020c5ae
niladic <git@nil.choron.cc>
2020-06-08 15:51:55 +0200

    Move Json formats to serializers.

--- app/controllers/MandatController.scala
+++ app/controllers/MandatController.scala
@@ -2,16 +2,15 @@ package controllers
 
 import actions.LoginAction
 import constants.Constants
-import helper.StringHelper
 import java.util.UUID
 import javax.inject.{Inject, Singleton}
 import models.{Error, EventType, Organisation, Sms}
 import models.mandat.{Mandat, SmsMandatInitiation}
 import org.webjars.play.WebJarsUtil
 import play.api.mvc.{Action, AnyContent, InjectedController, PlayBodyParsers}
-import play.api.libs.json._
-import play.api.libs.functional.syntax._
+import play.api.libs.json.{JsError, JsString, JsValue, Json}
 import scala.concurrent.{ExecutionContext, Future}
+import serializers.JsonFormats._
 import services.{
   EventService,
   MandatService,
@@ -37,54 +36,6 @@ case class MandatController @Inject() (
 )(implicit val ec: ExecutionContext, webJarsUtil: WebJarsUtil)
     extends InjectedController
     with UserOperators {
-  import serializers.DataModel.SmsFormats._
-
-  implicit val mandatIdReads: Reads[Mandat.Id] =
-    implicitly[Reads[UUID]].map(Mandat.Id.apply)
-
-  implicit val mandatIdWrites: Writes[Mandat.Id] =
-    implicitly[Writes[UUID]].contramap((id: Mandat.Id) => id.underlying)
-
-  implicit val mandatWrites: Writes[Mandat] = Json.writes[Mandat]
-
-  private val nameValidationRegex = """\p{L}[\p{L}\p{Z}\p{P}]{0,200}""".r
-
-  private val looseDateValidationRegex = """[\p{L}\p{Z}\p{P}\p{N}]{1,100}""".r
-
-  private val localPhoneRegex = """\d{10}""".r
-
-  private val nameValidator: Reads[String] = implicitly[Reads[String]]
-    .map(StringHelper.commonStringInputNormalization)
-    .filter(JsonValidationError("Nom invalide"))(nameValidationRegex.matches)
-
-  private val birthDateValidator: Reads[String] = implicitly[Reads[String]]
-    .map(StringHelper.commonStringInputNormalization)
-    .filter(JsonValidationError("Date de naissance invalide"))(looseDateValidationRegex.matches)
-
-  private val phoneValidator: Reads[String] = implicitly[Reads[String]]
-    .map(StringHelper.stripSpaces)
-    .filter(JsonValidationError("Téléphone invalide"))(localPhoneRegex.matches)
-
-  /** Normalize and validate all fields for security. */
-  implicit val smsMandatInitiationFormat: Format[SmsMandatInitiation] =
-    (JsPath \ "prenom")
-      .format[String](nameValidator)
-      .and((JsPath \ "nom").format[String](nameValidator))
-      .and((JsPath \ "birthDate").format[String](birthDateValidator))
-      .and((JsPath \ "phoneNumber").format[String](phoneValidator))(
-        SmsMandatInitiation.apply,
-        unlift(SmsMandatInitiation.unapply)
-      )
-
-  private def jsonInternalServerError =
-    InternalServerError(
-      Json.obj(
-        "message" -> JsString(
-          "Une erreur est survenue sur le serveur. " +
-            s"Si le problème persiste, pouvez contacter l’équipe A+ : ${Constants.supportEmail}."
-        )
-      )
-    )
 
   /** To have a somewhat consistent process:
     * - a SMS can fail for various reasons, even after having received a 2xx response from the api provider
--- app/serializers/JsonFormats.scala
+++ app/serializers/JsonFormats.scala
@@ -1,10 +1,13 @@
 package serializers
 
+import constants.Constants
+import helper.{StringHelper, UUIDHelper}
 import java.util.UUID
-
-import helper.UUIDHelper
+import models.mandat.{Mandat, SmsMandatInitiation}
 import play.api.libs.json.Json.JsValueWrapper
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
+import play.api.mvc.Results.InternalServerError
 
 object JsonFormats {
   implicit val jsonConfiguration = JsonConfiguration(naming = JsonNaming.SnakeCase)
@@ -29,4 +32,57 @@ object JsonFormats {
   }
 
   implicit val mapUUIDFormat = Format(mapUUIDReads, mapUUIDWrites)
+
+  //
+  // Mandat
+  //
+  import serializers.DataModel.SmsFormats._
+
+  implicit val mandatIdReads: Reads[Mandat.Id] =
+    implicitly[Reads[UUID]].map(Mandat.Id.apply)
+
+  implicit val mandatIdWrites: Writes[Mandat.Id] =
+    implicitly[Writes[UUID]].contramap((id: Mandat.Id) => id.underlying)
+
+  implicit val mandatWrites: Writes[Mandat] = Json.writes[Mandat]
+
+  private val nameValidationRegex = """\p{L}[\p{L}\p{Z}\p{P}]{0,200}""".r
+
+  private val looseDateValidationRegex = """[\p{L}\p{Z}\p{P}\p{N}]{1,100}""".r
+
+  private val localPhoneRegex = """\d{10}""".r
+
+  private val nameValidator: Reads[String] = implicitly[Reads[String]]
+    .map(StringHelper.commonStringInputNormalization)
+    .filter(JsonValidationError("Nom invalide"))(nameValidationRegex.matches)
+
+  private val birthDateValidator: Reads[String] = implicitly[Reads[String]]
+    .map(StringHelper.commonStringInputNormalization)
+    .filter(JsonValidationError("Date de naissance invalide"))(looseDateValidationRegex.matches)
+
+  private val phoneValidator: Reads[String] = implicitly[Reads[String]]
+    .map(StringHelper.stripSpaces)
+    .filter(JsonValidationError("Téléphone invalide"))(localPhoneRegex.matches)
+
+  /** Normalize and validate all fields for security. */
+  implicit val smsMandatInitiationFormat: Format[SmsMandatInitiation] =
+    (JsPath \ "prenom")
+      .format[String](nameValidator)
+      .and((JsPath \ "nom").format[String](nameValidator))
+      .and((JsPath \ "birthDate").format[String](birthDateValidator))
+      .and((JsPath \ "phoneNumber").format[String](phoneValidator))(
+        SmsMandatInitiation.apply,
+        unlift(SmsMandatInitiation.unapply)
+      )
+
+  def jsonInternalServerError =
+    InternalServerError(
+      Json.obj(
+        "message" -> JsString(
+          "Une erreur est survenue sur le serveur. " +
+            s"Si le problème persiste, pouvez contacter l’équipe A+ : ${Constants.supportEmail}."
+        )
+      )
+    )
+
 }
