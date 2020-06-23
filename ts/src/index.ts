8309d92ceafba1a8688a6ad744f04a9edb86796e
niladic <git@nil.choron.cc>
2020-06-23 10:55:16 +0200

    Ajout d'une pipeline pour compiler TypeScript

---
+++ ts/src/index.ts
@@ -0,0 +1,6 @@
+// Polyfills
+// See https://github.com/ryanelian/ts-polyfill/tree/master/lib
+import 'ts-polyfill/lib/es2015-core';
+
+// Our scripts
+import "./mdl-extensions.ts"
---
+++ ts/webpack/webpack.common.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+//
+// Save output JS in public/dist/js/index.js
+//
+module.exports = {
+  entry: {
+    index: path.join(__dirname, '../src/index.ts')
+  },
+  output: {
+    path: path.join(__dirname, '../../public/dist/js'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  }
+};
