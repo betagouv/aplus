c0eb943b4098e7d2f7ad9eed78ff71b3260b22b8
mdulac <mat.dulac@gmail.com>
2020-10-22 18:14:11 +0200

    const => var (due to IE11)

--- typescript/src/validateAccount.ts
+++ typescript/src/validateAccount.ts
@@ -1,9 +1,9 @@
-const firstnameValue = <HTMLInputElement | null>document.getElementById('firstnameValue');
-const lastnameValue = <HTMLInputElement | null>document.getElementById('lastnameValue');
-const qualiteValue = <HTMLInputElement | null>document.getElementById('qualiteValue');
-const sharedAccountValue = <HTMLInputElement | null>document.getElementById('sharedAccountValue');
+var firstnameValue = <HTMLInputElement | null>document.getElementById('firstnameValue');
+var lastnameValue = <HTMLInputElement | null>document.getElementById('lastnameValue');
+var qualiteValue = <HTMLInputElement | null>document.getElementById('qualiteValue');
+var sharedAccountValue = <HTMLInputElement | null>document.getElementById('sharedAccountValue');
 
-const checkbox = <HTMLInputElement | null>document.querySelector("#checkbox-charte");
+var checkbox = <HTMLInputElement | null>document.querySelector("#checkbox-charte");
 
 function formCanBeSubmitted(sharedAccount: Boolean) {
     return checkbox && checkbox.checked && (sharedAccount ? true :
@@ -14,19 +14,19 @@ function formCanBeSubmitted(sharedAccount: Boolean) {
 
 function addInputEvent(el: Element, sharedAccount: Boolean) {
     el && el.addEventListener(`input`, function () {
-        const e = <HTMLInputElement | null>document.querySelector("#validation")
+        var e = <HTMLInputElement | null>document.querySelector("#validation")
         e.disabled = !formCanBeSubmitted(sharedAccount);
     });
 }
 
 function addClickEvent(el: Element, sharedAccount: Boolean) {
     el.addEventListener(`click`, function () {
-        const e = <HTMLInputElement | null>document.querySelector("#validation");
+        var e = <HTMLInputElement | null>document.querySelector("#validation");
         e.disabled = !formCanBeSubmitted(sharedAccount);
     });
 }
 
-const sharedAccount = sharedAccountValue && sharedAccountValue.value !== "false";
+var sharedAccount = sharedAccountValue && sharedAccountValue.value !== "false";
 
 checkbox && addClickEvent(checkbox, sharedAccount);
 
