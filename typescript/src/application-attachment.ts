4630f2c1f7286863e1d5e9cffc13ffcf57ebd11a
niladic <git@nil.choron.cc>
2020-10-28 15:28:25 +0100

    Fix missing polyfill for NodeList

--- typescript/src/application-attachment.ts
+++ typescript/src/application-attachment.ts
@@ -1,55 +1,55 @@
 interface ApplicationAttachment {
-    list: HTMLElement
-    fileInputCount: number
-    idSequence: number
-    increaseOrDecreaseFileInputList: (EventListener) => any
+  list: HTMLElement
+  fileInputCount: number
+  idSequence: number
+  increaseOrDecreaseFileInputList: (EventListener) => any
 }
 
 let attachment: any = {}
 
 
 const createAttachment = (list: HTMLElement, count: number, sequence: number): ApplicationAttachment => {
-    return {
-        list,
-        fileInputCount: count,
-        idSequence: sequence, // In order to avoid id collisions
-        increaseOrDecreaseFileInputList: event => {
-            if (event.target.files.length > 0) {
-                // If a new file is added, add a new file input.
-                const li: HTMLLIElement = document.createElement("li");
-                const input: HTMLInputElement = document.createElement("input");
-                input.setAttribute("type", "file");
-                input.setAttribute("name", "file[" + (attachment.idSequence++) + "]");
-                attachment.fileInputCount++;
-                input.addEventListener("change", attachment.increaseOrDecreaseFileInputList);
-                attachment.list.appendChild(li);
-                li.appendChild(input);
-            } else {
-                // If a previous file is unset for upload, remove the corresponding input file.
-                if (attachment.fileInputCount > 1) {
-                    event.target.parentElement.parentElement.removeChild(event.target.parentElement);
-                    attachment.fileInputCount--;
-                }
-            }
+  return {
+    list,
+    fileInputCount: count,
+    idSequence: sequence, // In order to avoid id collisions
+    increaseOrDecreaseFileInputList: event => {
+      if (event.target.files.length > 0) {
+        // If a new file is added, add a new file input.
+        const li: HTMLLIElement = document.createElement("li");
+        const input: HTMLInputElement = document.createElement("input");
+        input.setAttribute("type", "file");
+        input.setAttribute("name", "file[" + (attachment.idSequence++) + "]");
+        attachment.fileInputCount++;
+        input.addEventListener("change", attachment.increaseOrDecreaseFileInputList);
+        attachment.list.appendChild(li);
+        li.appendChild(input);
+      } else {
+        // If a previous file is unset for upload, remove the corresponding input file.
+        if (attachment.fileInputCount > 1) {
+          event.target.parentElement.parentElement.removeChild(event.target.parentElement);
+          attachment.fileInputCount--;
         }
+      }
     }
+  }
 }
 
 const main = (list: HTMLElement | null) => {
-    if (list != null) {
-        const attachment = createAttachment(list, 1, 0)
-        // For all file inputs, add a listener that adds a new file input to allow
-        // for more attachments to be made.
-        attachment.list.querySelectorAll("input").forEach(input => {
-            input.addEventListener("change", attachment.increaseOrDecreaseFileInputList);
-        });
+  if (list != null) {
+    const attachment = createAttachment(list, 1, 0)
+    // For all file inputs, add a listener that adds a new file input to allow
+    // for more attachments to be made.
+    attachment.list.querySelectorAll("input").forEach(input => {
+      input.addEventListener("change", attachment.increaseOrDecreaseFileInputList);
+    });
 
-        return attachment;
-    }
+    return attachment;
+  }
 }
 
 window.document.addEventListener("DOMContentLoaded", _ => {
-    "use strict";
-    const attachmentList: HTMLElement | null = document.getElementById("attachment-list");
-    attachment = main(attachmentList)
+  "use strict";
+  const attachmentList: HTMLElement | null = document.getElementById("attachment-list");
+  attachment = main(attachmentList)
 }, false);
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,8 @@
 // Polyfills
 // See https://github.com/ryanelian/ts-polyfill/tree/master/lib
 import 'ts-polyfill/lib/es2015-core';
+// This adds NodeList.forEach, etc.
+import 'core-js/web/dom-collections';
 
 // Our scripts
 import "./admin.ts"
