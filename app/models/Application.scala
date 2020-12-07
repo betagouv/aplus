08bc8c3c2822a7896518e0a03791a6560da173a0
mdulac <mat.dulac@gmail.com>
2020-12-07 11:42:27 +0100

    Remove unused import

--- app/models/Application.scala
+++ app/models/Application.scala
@@ -2,7 +2,6 @@ package models
 
 import cats.syntax.all._
 import cats.{Eq, Show}
-import helper.BooleanHelper.not
 import models.Answer.AnswerType.ApplicationProcessed
 import models.Application.SeenByUser
 import models.Application.Status.{Archived, New, Processed, Processing, Sent, ToArchive}
