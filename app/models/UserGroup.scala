6bbf5c02cc59339feff74b298b461e294d1ff612
niladic <git@nil.choron.cc>
2021-01-12 20:09:08 +0100

    Ajoute les infos utilisateur et groupe dans les logs

--- app/models/UserGroup.scala
+++ app/models/UserGroup.scala
@@ -1,5 +1,7 @@
 package models
 
+import cats.syntax.all._
+import helper.StringHelper.withQuotes
 import java.time.ZonedDateTime
 import java.util.UUID
 
@@ -24,12 +26,32 @@ case class UserGroup(
       .flatMap(Organisation.byId)
       .orElse(Organisation.deductedFromName(name))
 
+  lazy val nameLog: String = withQuotes(name)
+  lazy val descriptionLog: String = description.map(withQuotes).getOrElse("<vide>")
+  lazy val emailLog: String = email.map(withQuotes).getOrElse("<vide>")
+  lazy val organisationLog: String = organisation.map(_.id).getOrElse("<vide>")
+  lazy val areaIdsLog: String = areaIds.mkString(", ")
+
   lazy val toLogString: String =
-    s"[Id: '$id' ; " +
-      s"Nom : '$name' ; " +
-      s"""Description : '${description.getOrElse("<vide>")}' ; """ +
-      s"""BAL : '${email.getOrElse("<vide>")}' ; """ +
-      s"""Organisme : ${organisation.map(_.id).getOrElse("<vide>")} ; """ +
-      s"""Territoires : ${areaIds.mkString(", ")}]"""
+    "[" + List[(String, String)](
+      ("Id", id.toString),
+      ("Nom", nameLog),
+      ("Description", descriptionLog),
+      ("BAL", emailLog),
+      ("Organisme", organisationLog),
+      ("Territoires", areaIdsLog)
+    ).map { case (fieldName, value) => s"$fieldName : $value" }.mkString(" | ") + "]"
+
+  def toDiffLogString(other: UserGroup): String = {
+    val diffs: List[String] = List[(String, Boolean, String, String)](
+      ("Id", id =!= other.id, id.toString, other.id.toString),
+      ("Nom", name =!= other.name, nameLog, other.nameLog),
+      ("Description", description =!= other.description, descriptionLog, other.descriptionLog),
+      ("BAL", email =!= other.email, emailLog, other.emailLog),
+      ("Organisme", organisation =!= other.organisation, organisationLog, other.organisationLog),
+      ("Territoires", areaIds =!= other.areaIds, areaIdsLog, other.areaIdsLog),
+    ).collect { case (name, true, thisValue, thatValue) => s"$name : $thisValue -> $thatValue" }
+    "[" + diffs.mkString(" | ") + "]"
+  }
 
 }
