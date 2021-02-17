7b52b7516684f5021ca877ecc3d9f84eda8492d0
niladic <git@nil.choron.cc>
2021-02-17 12:14:35 +0100

    Add nav arrow for errors on CSV import page

---
+++ app/views/helpers/csv.scala
@@ -0,0 +1,54 @@
+package views.helpers
+
+import cats.syntax.all._
+import play.api.data.{Form, FormError}
+import play.api.i18n.Messages
+import scalatags.Text.all._
+
+object csv {
+
+  val errorsId: String = s"all-errors"
+  def groupTitleId(groupIndex: Int): String = s"group-index-$groupIndex"
+
+  def allErrors(form: Form[_])(implicit messagesProvider: Messages): Frag =
+    if (form.hasErrors)
+      div(
+        id := errorsId,
+        cls := "mdl-cell mdl-cell--12-col",
+        form.errors.map { error =>
+          displayError(error)
+        }
+      )
+    else
+      div(id := errorsId)
+
+  private val groupIndexRegex = """^groups\[(\d+)\].*""".r
+
+  def displayError(error: FormError)(implicit messagesProvider: Messages): Frag = {
+    val linkToGroup: Frag =
+      error.key match {
+        case groupIndexRegex(rawIndex) =>
+          val index = rawIndex.toInt
+          a(
+            cls := "mdl-navigation__link aplus-color-text--black",
+            href := s"#${groupTitleId(index)}",
+            i(cls := "material-icons", role := "presentation", "arrow_downward")
+          )
+        case _ =>
+          ()
+      }
+
+    div(
+      cls := "notification notification--error",
+      div(
+        cls := "mdl-cell mdl-cell--12-col single--align-items-center-important",
+        linkToGroup,
+        s"${error.format}",
+        s" (Champ du formulaire : ${error.key})",
+        if (error.args.isEmpty) ""
+        else " (Arguments : " + error.args.mkString(",") + ")"
+      )
+    )
+  }
+
+}
