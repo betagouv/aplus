01ab44a55ac91b58dba17b9e5745e83d1bd53832
niladic <git@nil.choron.cc>
2021-03-25 12:07:22 +0100

    Update 48.sql
    limite à 100 chars

--- conf/evolutions/default/48.sql
+++ conf/evolutions/default/48.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(250);
+ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(100);
 
 # --- !Downs
 ALTER TABLE "user_group" ALTER COLUMN "name" SET DATA TYPE character varying(60) USING SUBSTR("name", 1, 60);
