651f59d7a36d3b7e9a604ada5aef4a810522b2a6
niladic <git@nil.choron.cc>
2023-06-14 14:45:45 +0200

    Stop les emails de BALs quand un email utilisateur a déjà été envoyé

--- app/services/NotificationService.scala
+++ app/services/NotificationService.scala
@@ -117,13 +117,17 @@ class NotificationService @Inject() (
       }
       .foreach(email => emailsService.sendBlocking(email, EmailPriority.Normal))
 
+    val usersEmails: Set[String] = users.map(_.email).toSet
+
     // Send emails to groups
     allGroups
       .collect {
-        case group if alreadyPresentGroupIds.contains(group.id) =>
-          generateNotificationBALEmail(application, answer.some, users)(group)
-        case group if !alreadyPresentGroupIds.contains(group.id) =>
-          generateNotificationBALEmail(application, Option.empty[Answer], users)(group)
+        case group @ UserGroup(id, _, _, _, _, _, _, Some(email), _, _)
+            if !usersEmails.contains(email) =>
+          if (alreadyPresentGroupIds.contains(id))
+            generateNotificationBALEmail(application, answer.some, users)(group)
+          else
+            generateNotificationBALEmail(application, Option.empty[Answer], users)(group)
       }
       .foreach(email => emailsService.sendBlocking(email, EmailPriority.Normal))
 
