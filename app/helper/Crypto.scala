c5c6d0f62040ae2af5ad8d7543d8acc5238a1baa
niladic <git@nil.choron.cc>
2024-08-07 09:49:23 +0200

    remove unused code

--- app/helper/Crypto.scala
+++ app/helper/Crypto.scala
@@ -5,7 +5,7 @@ import java.security.SecureRandom
 import java.util.{Arrays, Base64}
 import javax.crypto.{Cipher, KeyGenerator, SecretKey}
 import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 /** This singleton regroups high level functions using AEAD with ChaCha20-Poly1305
   *
@@ -22,9 +22,6 @@ import scala.util.{Failure, Success, Try}
   *     - Transitioning to New Algorithms and Key Sizes in Systems If the protected data is
   *       retained, it should be re-protected using an approved algorithm and key size that will
   *       protect the information for the remainder of its security life.
-  *   - Here `KeySet` is a pair of (encrypting key, old keys previously used to encrypt) It is
-  *     useful for smooth key rotation and fields that do not require key rotation because they are
-  *     wiped after a fixed amount of time.
   */
 object Crypto {
 
@@ -43,72 +40,6 @@ object Crypto {
   val base64Encoder = Base64.getEncoder()
 
   case class Key(key: SecretKey)
-
-  /** A `KeySet` holds an encrypt/decrypt key `validKey` and 'decrypt-only' keys `oldKeys`. This is
-    * used in order to smoothly rotate keys. `oldKeys` are assumed to be insecure for encrypting new
-    * data (due to scheduled rotation or compromise).
-    */
-  case class KeySet(validKey: Key, oldKeys: List[Key])
-
-  /** This class wraps the ciphertext in order to avoid keeping the plaintext in memory when this is
-    * not necessary.
-    */
-  case class EncryptedField private (cipherTextBase64: String, aad: String) {
-
-    private def decryptWithOldKeys(keyset: KeySet, noOldKeysError: Throwable): Try[String] =
-      keyset.oldKeys.foldLeft[Try[String]](Failure(noOldKeysError)) { case (acc, oldKey) =>
-        if (acc.isSuccess)
-          acc
-        else
-          decryptField(cipherTextBase64, aad, oldKey)
-      }
-
-    def decrypt(keyset: KeySet): Try[String] =
-      decryptField(cipherTextBase64, aad, keyset.validKey)
-        .recoverWith { case firstError: javax.crypto.AEADBadTagException =>
-          decryptWithOldKeys(keyset, firstError)
-        }
-
-    /** Does nothing (returns None) if the encrypting key has been used to encrypt the field.
-      * Decrypt and encrypt if one of the old keys has been used to encrypt the field. Used for key
-      * rotation.
-      *
-      * Returns None if nothing has been done, Some if key rotation has occured.
-      *
-      * `encryptIfPlainText` is used for updating a plain text field to a encrypted field. The field
-      * is encrypted if it fails base64 decoding or decryption.
-      */
-    def updateKey(keyset: KeySet, encryptIfPlainText: Boolean): Try[Option[EncryptedField]] =
-      decryptField(cipherTextBase64, aad, keyset.validKey) match {
-        case Success(_) => Success(None)
-        case Failure(firstError: javax.crypto.AEADBadTagException) =>
-          decryptWithOldKeys(keyset, firstError) match {
-            case Success(plainText) =>
-              EncryptedField.fromPlainText(plainText, aad, keyset).map(Some.apply)
-            case Failure(e) =>
-              if (encryptIfPlainText)
-                EncryptedField.fromPlainText(cipherTextBase64, aad, keyset).map(Some.apply)
-              else
-                Failure(e)
-          }
-        case Failure(e) =>
-          if (encryptIfPlainText)
-            EncryptedField.fromPlainText(cipherTextBase64, aad, keyset).map(Some.apply)
-          else
-            Failure(e)
-      }
-
-  }
-
-  object EncryptedField {
-
-    def fromPlainText(plainText: String, aad: String, keyset: KeySet): Try[EncryptedField] =
-      encryptField(plainText, aad, keyset.validKey).map(ct => EncryptedField(ct, aad))
-
-    def fromCipherText(cipherText: String, aad: String): EncryptedField =
-      EncryptedField(cipherText, aad)
-
-  }
 
   def generateRandomKey(): Key = {
     val generator = KeyGenerator.getInstance(KEYGENERATOR_ALGORITHM)
@@ -139,25 +70,25 @@ object Crypto {
     (cipher, nonce)
   }
 
-  /** Returns (nonce || cipherText) */
-  def encryptBytes(plainText: Array[Byte], aad: String, key: Key): Try[Array[Byte]] = Try {
+  /** Returns (nonce || ciphertext) */
+  def encryptBytes(plaintext: Array[Byte], aad: String, key: Key): Try[Array[Byte]] = Try {
     val (cipher, nonce) = encryptCipher(aad, key)
 
-    // Always plainText.length + 16 bytes of ChaCha20-Poly1305 authentication tag
-    val outputSize = cipher.getOutputSize(plainText.length)
+    // Always plaintext.length + 16 bytes of ChaCha20-Poly1305 authentication tag
+    val outputSize = cipher.getOutputSize(plaintext.length)
 
-    // We allocate only one Array for the cipherText
-    val cipherTextWithNonce = Arrays.copyOf(nonce, NONCE_SIZE_BYTES + outputSize)
+    // We allocate only one Array for the ciphertext
+    val ciphertextWithNonce = Arrays.copyOf(nonce, NONCE_SIZE_BYTES + outputSize)
 
     // Fills the result Array after the nonce
-    cipher.doFinal(plainText, 0, plainText.length, cipherTextWithNonce, NONCE_SIZE_BYTES)
+    cipher.doFinal(plaintext, 0, plaintext.length, ciphertextWithNonce, NONCE_SIZE_BYTES)
 
-    cipherTextWithNonce
+    ciphertextWithNonce
   }
 
-  /** Returns base64(nonce || cipherText) */
-  def encryptField(plainText: String, aad: String, key: Key): Try[String] =
-    encryptBytes(plainText.getBytes(StandardCharsets.UTF_8), aad, key)
+  /** Returns base64(nonce || ciphertext) */
+  def encryptField(plaintext: String, aad: String, key: Key): Try[String] =
+    encryptBytes(plaintext.getBytes(StandardCharsets.UTF_8), aad, key)
       .map(base64Encoder.encodeToString)
 
   private def decryptCipher(nonce: Array[Byte], aad: String, key: Key): Cipher = {
@@ -167,23 +98,23 @@ object Crypto {
     cipher
   }
 
-  def decryptBytes(cipherTextWithNonce: Array[Byte], aad: String, key: Key): Try[Array[Byte]] =
+  def decryptBytes(ciphertextWithNonce: Array[Byte], aad: String, key: Key): Try[Array[Byte]] =
     Try {
-      val nonce = Arrays.copyOf(cipherTextWithNonce, NONCE_SIZE_BYTES)
+      val nonce = Arrays.copyOf(ciphertextWithNonce, NONCE_SIZE_BYTES)
       val cipher = decryptCipher(nonce, aad, key)
-      val plainText = {
+      val plaintext = {
         val inputOffset = NONCE_SIZE_BYTES
-        val inputLen = cipherTextWithNonce.length - NONCE_SIZE_BYTES
+        val inputLen = ciphertextWithNonce.length - NONCE_SIZE_BYTES
         // We avoid allocating a new Array
-        cipher.doFinal(cipherTextWithNonce, inputOffset, inputLen)
+        cipher.doFinal(ciphertextWithNonce, inputOffset, inputLen)
       }
-      plainText
+      plaintext
     }
 
-  def decryptField(cipherTextBase64: String, aad: String, key: Key): Try[String] =
-    Try(base64Decoder.decode(cipherTextBase64))
-      .flatMap(cipherTextWithNonce => decryptBytes(cipherTextWithNonce, aad, key))
-      .flatMap(plainText => Try(new String(plainText, StandardCharsets.UTF_8)))
+  def decryptField(ciphertextBase64: String, aad: String, key: Key): Try[String] =
+    Try(base64Decoder.decode(ciphertextBase64))
+      .flatMap(ciphertextWithNonce => decryptBytes(ciphertextWithNonce, aad, key))
+      .flatMap(plaintext => Try(new String(plaintext, StandardCharsets.UTF_8)))
 
   /** ******************** // Stream Utilities // *********************
     *
@@ -240,10 +171,10 @@ object Crypto {
       _.chunkAll
         .evalMap { chunk =>
           IO {
-            val (nonce, cipherText) = chunk.splitAt(NONCE_SIZE_BYTES)
+            val (nonce, ciphertext) = chunk.splitAt(NONCE_SIZE_BYTES)
             val cipher = decryptCipher(nonce.toArray, aad, key)
-            val plainText = cipher.doFinal(cipherText.toArray)
-            Chunk.array(plainText)
+            val plaintext = cipher.doFinal(ciphertext.toArray)
+            Chunk.array(plaintext)
           }
         }
         .unchunks
