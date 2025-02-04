b62d5d0cc1e36d91962613552be15157fbc64e38
niladic <git@nil.choron.cc>
2025-02-04 18:17:25 +0100

    fix

--- scripts/copy-scalingo-review-app-db.sh
+++ scripts/copy-scalingo-review-app-db.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-echo "Running the Scalingo Review App DB Copy Script"
+echo "Running Scalingo review app DB copy script"
 
 if [ "$IS_REVIEW_APP" = "true" ]; then
+  dbclient-fetcher pgsql
+
   echo "Copying review app db"
 
   pg_dump --clean --if-exists --format c --dbname "${PARENT_APP_DATABASE_URL}" --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname "${SCALINGO_POSTGRESQL_URL}"
