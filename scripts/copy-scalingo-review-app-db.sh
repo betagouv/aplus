d85f6833c05b801abe436604b799e108d7621a29
niladic <git@nil.choron.cc>
2025-02-04 18:59:57 +0100

    wip

--- scripts/copy-scalingo-review-app-db.sh
+++ scripts/copy-scalingo-review-app-db.sh
@@ -7,7 +7,7 @@ if [ "$IS_REVIEW_APP" = "true" ]; then
 
   echo "Copying review app db"
 
-  pg_dump --clean --if-exists --format c --dbname "${PARENT_APP_DATABASE_URL}" --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname "${SCALINGO_POSTGRESQL_URL}"
+  /app/bin/pg_dump --clean --if-exists --format c --dbname "${PARENT_APP_DATABASE_URL}" --no-owner --no-privileges --no-comments --exclude-schema 'information_schema' --exclude-schema '^pg_*' | /app/bin/pg_restore --clean --if-exists --no-owner --no-privileges --no-comments --dbname "${SCALINGO_POSTGRESQL_URL}"
 
   echo "Done"
 else
