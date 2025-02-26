7d53f485b6d79633edd760a5790c0ca12feab746
niladic <git@nil.choron.cc>
2025-02-26 16:32:59 +0100

    add field to materialized view

--- conf/evolutions/default/81.sql
+++ conf/evolutions/default/81.sql
@@ -3,8 +3,62 @@
 ALTER TABLE user_group ADD is_in_france_services_network BOOLEAN NOT NULL DEFAULT true;
 ALTER TABLE application ADD is_in_france_services_network BOOLEAN NOT NULL DEFAULT true;
 
+DROP MATERIALIZED VIEW application_metadata;
+
+CREATE MATERIALIZED VIEW application_metadata AS
+SELECT
+    id,
+    creation_date,
+    creator_user_name,
+    creator_user_id,
+    creator_group_id,
+    creator_group_name,
+    invited_users,
+    is_in_france_services_network,
+    area,
+    irrelevant,
+    internal_id,
+    closed,
+    seen_by_user_ids,
+    usefulness,
+    closed_date,
+    expert_invited,
+    has_selected_subject,
+    category,
+    mandat_type,
+    mandat_date,
+    personal_data_wiped
+FROM application;
+
+
 
 -- !Downs
+
+DROP MATERIALIZED VIEW application_metadata;
+
+CREATE MATERIALIZED VIEW application_metadata AS
+SELECT
+    id,
+    creation_date,
+    creator_user_name,
+    creator_user_id,
+    creator_group_id,
+    creator_group_name,
+    invited_users,
+    area,
+    irrelevant,
+    internal_id,
+    closed,
+    seen_by_user_ids,
+    usefulness,
+    closed_date,
+    expert_invited,
+    has_selected_subject,
+    category,
+    mandat_type,
+    mandat_date,
+    personal_data_wiped
+FROM application;
 
 ALTER TABLE application DROP is_in_france_services_network;
 ALTER TABLE user_group DROP is_in_france_services_network;
